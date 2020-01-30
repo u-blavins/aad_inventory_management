17e851e3e2b6429a5727a4b096657d39086c85be
wajsters <32671145+wajsters@users.noreply.github.com>
2020-01-30 16:48:40 +0000

    Improved login functionality and implemented register feature
    Login now uses a "GetUser" SPROC that returns a user object to check whether they are staff or customer.
    
    Register now allows users to register to the database (Currently inserts straight into the table -> needs approve registration functionality implemented)

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     return render_template('auth.html')
 
 @app.route('/auth/login', methods=['POST'])
-def Login():
+def login():
     if request.method == 'POST':
         conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                     'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
@@ -23,17 +23,59 @@ def Login():
         EXEC [usr].[UserLogin] @Email = ?, @Password= ?,@responseMessage = @out OUTPUT;
         SELECT @out AS the_output;
         """
-        params = (request.form['email'], request.form['password'])
+        userEmail = request.form['email']
+        params = (userEmail, request.form['password'])
         cursor = conn.cursor()
         cursor.execute(sql, params)
         result = cursor.fetchone()
         if "Login successful" == result[0]:
-            return redirect('/home/admin')
+            sql = "EXEC [usr].[getUser] @Email = ?"
+            params = userEmail
+            cursor.execute(sql, params)
+            getRow = cursor.fetchone()
+            isAdmin = getRow[4]
+            conn.close()
+            if isAdmin:
+                return redirect('/home/admin')
+            else:
+                return redirect('/home/basket')
+
+    conn.close()
     return redirect('/auth')
-            
+
+@app.route('/auth/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                              'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
+                              'Database=StoreManagement;'
+                              'uid=admin;'
+                              'pwd=letsusefirebase;')
+        sql = """
+        DECLARE @out nvarchar(max);
+        EXEC [usr].[CreateUser] @Email = ?, @Password= ?, @FirstName = ?, @LastName = ?, 
+        @DepartmentCode = ?, @isStaff = ?, @responseMessage = @out OUTPUT;
+        SELECT @out AS the_output;
+        """
+        params = (request.form['regEmail'], request.form['regPassword'], request.form['regFirstname'], request.form['regLastname']
+                  , request.form['departmentCode'], request.form['accountType'])
+        cursor = conn.cursor()
+        cursor.execute(sql, params)
+        result = cursor.fetchone()
+        if "Success" == result[0]:
+            conn.commit()
+            conn.close()
+        else:
+            conn.close()
+    return redirect('/auth')
+
 @app.route('/home/admin')
 def admin():
     return render_template('adminArea.html')
+
+@app.route('/home/basket')
+def customer():
+    return render_template('addItemToBasket.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
