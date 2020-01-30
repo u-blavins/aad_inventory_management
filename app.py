3b4ccb1a32904191d48225f9f132586f9debd61b
u-blavins <usama.blavins1@gmail.com>
2020-01-30 15:04:35 +0000

    Update app.py with login

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
 from flask import Flask, render_template, redirect, request, session, url_for
-import pypyodbc
+import pyodbc
 import os
 
 app = Flask(__name__)
 app.debug = True
 
-
-# try:
-#     #creating connection Object which will contain SQL Server Connection
-#     connection = pypyodbc.connect('')
-#     print("Connected.")
-#     #closing connection
-#     connection.close()
-# except Exception as ex:
-#     print(ex)
-
 @app.route('/')
+@app.route('/auth')
 def main():
     return render_template('auth.html')
 
+@app.route('/auth/login', methods=['POST'])
+def Login():
+    if request.method == 'POST':
+        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                    'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
+                    'Database=StoreManagement;'
+                    'uid=admin;'
+                    'pwd=letsusefirebase;')
+        sql = """
+        DECLARE @out nvarchar(max);
+        EXEC [usr].[UserLogin] @Email = ?, @Password= ?,@responseMessage = @out OUTPUT;
+        SELECT @out AS the_output;
+        """
+        params = (request.form['email'], request.form['password'])
+        cursor = conn.cursor()
+        rc = cursor.execute(sql, params)
+        if "Login successful" in status:
+            return redirect('/home/admin')
+    return redirect('/auth')
+            
+@app.route('/home/admin')
+def admin():
+    return render_template('adminArea.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
