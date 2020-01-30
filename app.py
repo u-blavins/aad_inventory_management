d8d3d5f41e08521340c0ae8d4941090069633438
u-blavins <usama.blavins1@gmail.com>
2020-01-30 17:19:12 +0000

    Update app.py by refactor code to fit within PEP-8

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ def main():
 @app.route('/auth/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                    'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
-                    'Database=StoreManagement;'
-                    'uid=admin;'
-                    'pwd=letsusefirebase;')
+        conn = pyodbc.connect(
+            'Driver={ODBC Driver 17 for SQL Server};'
+            'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
+            'Database=StoreManagement;'
+            'uid=admin;'
+            'pwd=letsusefirebase;')
         sql = """
         DECLARE @out nvarchar(max);
         EXEC [usr].[UserLogin] @Email = ?, @Password= ?,@responseMessage = @out OUTPUT;
@@ -46,19 +47,25 @@ def login():
 @app.route('/auth/register', methods=['POST'])
 def register():
     if request.method == 'POST':
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                              'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
-                              'Database=StoreManagement;'
-                              'uid=admin;'
-                              'pwd=letsusefirebase;')
+        conn = pyodbc.connect(
+            'Driver={ODBC Driver 17 for SQL Server};'
+            'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
+            'Database=StoreManagement;'
+            'uid=admin;'
+            'pwd=letsusefirebase;')
         sql = """
         DECLARE @out nvarchar(max);
         EXEC [usr].[CreateUser] @Email = ?, @Password= ?, @FirstName = ?, @LastName = ?, 
         @DepartmentCode = ?, @isStaff = ?, @responseMessage = @out OUTPUT;
         SELECT @out AS the_output;
         """
-        params = (request.form['regEmail'], request.form['regPassword'], request.form['regFirstname'], request.form['regLastname']
-                  , request.form['departmentCode'], request.form['accountType'])
+        params = (
+            request.form['regEmail'],
+            request.form['regPassword'],
+            request.form['regFirstname'],
+            request.form['regLastname'],
+            request.form['departmentCode'],
+            request.form['accountType'])
         cursor = conn.cursor()
         cursor.execute(sql, params)
         result = cursor.fetchone()
