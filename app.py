c4c6992aca134a29e2e8ff298b8972af91d4aa3d
wajsters <32671145+wajsters@users.noreply.github.com>
2020-01-30 18:46:43 +0000

    Created Database class for handling sql calls

--- app.py
+++ app.py
@@ -1,64 +1,40 @@
 from flask import Flask, render_template, redirect, request, session, url_for
-import pyodbc
-import os
+from utils import Database
 
 app = Flask(__name__)
 app.debug = True
+
 
 @app.route('/')
 @app.route('/auth')
 def main():
     return render_template('auth.html')
 
+
 @app.route('/auth/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        conn = pyodbc.connect(
-            'Driver={ODBC Driver 17 for SQL Server};'
-            'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
-            'Database=StoreManagement;'
-            'uid=admin;'
-            'pwd=letsusefirebase;')
-        sql = """
-        DECLARE @out nvarchar(max);
-        EXEC [usr].[UserLogin] @Email = ?, @Password= ?,@responseMessage = @out OUTPUT;
-        SELECT @out AS the_output;
-        """
-        userEmail = request.form['email']
-        params = (userEmail, request.form['password'])
-        cursor = conn.cursor()
-        cursor.execute(sql, params)
-        result = cursor.fetchone()
-        if "Login successful" == result[0]:
-            sql = "EXEC [usr].[getUser] @Email = ?"
-            params = userEmail
-            cursor.execute(sql, params)
-            getRow = cursor.fetchone()
-            isAdmin = getRow[4]
-            conn.close()
-            if isAdmin:
+        sproc = "[usr].[UserLogin] @Email = ?, @Password= ?"
+        user_email = request.form['email']
+        params = (user_email, request.form['password'])
+        db = Database.Database()
+        result = db.execute_query(sproc, params)
+
+        if "Login successful" == result[0][0]:
+            sproc = "[usr].[getUser] @Email = ?"
+            params = user_email
+            result = db.execute_query(sproc, params)
+            is_admin = result[0][4]
+            if is_admin:
                 return redirect('/home/admin')
             else:
                 return redirect('/home/basket')
-
-    conn.close()
     return redirect('/auth')
+
 
 @app.route('/auth/register', methods=['POST'])
 def register():
     if request.method == 'POST':
-        conn = pyodbc.connect(
-            'Driver={ODBC Driver 17 for SQL Server};'
-            'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
-            'Database=StoreManagement;'
-            'uid=admin;'
-            'pwd=letsusefirebase;')
-        sql = """
-        DECLARE @out nvarchar(max);
-        EXEC [usr].[CreateUser] @Email = ?, @Password= ?, @FirstName = ?, @LastName = ?, 
-        @DepartmentCode = ?, @isStaff = ?, @responseMessage = @out OUTPUT;
-        SELECT @out AS the_output;
-        """
         params = (
             request.form['regEmail'],
             request.form['regPassword'],
@@ -66,24 +42,23 @@ def register():
             request.form['regLastname'],
             request.form['departmentCode'],
             request.form['accountType'])
-        cursor = conn.cursor()
-        cursor.execute(sql, params)
-        result = cursor.fetchone()
-        if "Success" == result[0]:
-            conn.commit()
-            conn.close()
-        else:
-            conn.close()
+
+        sproc = """[usr].[CreateUser] @Email = ?, @Password= ?, @FirstName = ?, @LastName = ?, 
+        @DepartmentCode = ?, @isStaff = ?"""
+        db = Database.Database()
+        db.execute_query(sproc, params)
     return redirect('/auth')
+
 
 @app.route('/home/admin')
 def admin():
     return render_template('adminArea.html')
 
+
 @app.route('/home/basket')
 def customer():
     return render_template('addItemToBasket.html')
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
--- utils/Database.py
+++ utils/Database.py
@@ -1,6 +1,25 @@
 import pyodbc
 
+
 class Database:
     """ Database Object to execute commands """
+    conn = pyodbc.connect(
+        'Driver={ODBC Driver 17 for SQL Server};'
+        'Server=secretsasquatchsociety.chefvdjywstx.eu-west-2.rds.amazonaws.com,1433;'
+        'Database=StoreManagement;'
+        'uid=admin;'
+        'pwd=letsusefirebase;')
 
-    
+    def execute_query(self, query, params):
+        sql = """
+            DECLARE @out nvarchar(max);
+            EXEC %s ,@responseMessage = @out OUTPUT;
+            SELECT @out AS the_output;         
+        """ % query
+
+        cursor = self.conn.cursor()
+        cursor.execute(sql, params)
+        result = cursor.fetchall()
+        cursor.commit()
+        return result
+
