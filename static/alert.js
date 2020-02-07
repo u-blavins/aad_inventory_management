4ac010372c76e29e0403a82ed05a3b72f6c8694a
N0689085 <32670704+N0689085@users.noreply.github.com>
2020-02-07 22:48:45 +0000

    error handling (staff department code)

---
+++ static/alert.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    alert("I am an alert box!");
+  }
+
+
+function myFunction() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
