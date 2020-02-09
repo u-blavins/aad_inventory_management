1e5d8771ef717a6773359e97904da6bc20956f05
wjones98 <38158543+wjones98@users.noreply.github.com>
2020-02-09 03:46:28 +0000

    View Purchase Orders Page Implemented

---
+++ models/PurchaseOrder.py
@@ -0,0 +1,92 @@
+from utils.Database import Database
+
+
+class PurchaseOrder:
+    """Purchase Order Model"""
+
+    @staticmethod
+    def get_purchase_orders_pending():
+        query = """
+            SELECT
+                [PurchaseOrderID],
+                [Email],
+                [GeneratedDate]
+            FROM
+                [itm].[PendingPurchaseOrders]
+        """
+        conn = Database.connect()
+        cursor = conn.cursor()
+        rows = Database.execute_query(query, cursor)
+        conn.close()
+
+        purchase_orders = []
+
+        for row in rows:
+            purchase_order = PurchaseOrder()
+            purchase_order.set_purchase_order_id(row[0])
+            purchase_order.set_generated_by(row[1])
+            purchase_order.set_generated_date(row[2])
+            purchase_orders.append(purchase_order)
+
+        return purchase_orders
+
+    @staticmethod
+    def get_purchase_orders_history():
+        query = """
+            SELECT
+                [PurchaseOrderID],
+                [Email],
+                [GeneratedDate],
+                [CompletionDate]
+            FROM
+                [itm].[PurchaseOrdersHistory]
+        """
+        conn = Database.connect()
+        cursor = conn.cursor()
+        rows = Database.execute_query(query, cursor)
+        conn.close()
+
+        purchase_orders = []
+
+        for row in rows:
+            purchase_order = PurchaseOrder()
+            purchase_order.set_purchase_order_id(row[0])
+            purchase_order.set_generated_by(row[1])
+            purchase_order.set_generated_date(row[2])
+            purchase_order.set_completion_date(row[3])
+            purchase_orders.append(purchase_order)
+
+        return purchase_orders
+
+    def __init__(self):
+        self.purchase_order_id = None
+        self.purchase_order = {}
+        return
+
+    def set_purchase_order_id(self, purchase_order_id):
+        self.purchase_order_id = purchase_order_id
+        return self
+
+    def get_purchase_order_id(self):
+        return self.purchase_order_id
+
+    def set_generated_by(self, generated_by):
+        self.purchase_order['generated_by'] = generated_by
+        return self
+
+    def get_generated_by(self):
+        return self.purchase_order['generated_by']
+
+    def set_generated_date(self, generated_date):
+        self.purchase_order['generated_date'] = generated_date
+        return self
+
+    def get_generated_date(self):
+        return self.purchase_order['generated_date']
+
+    def set_completion_date(self, completion_date):
+        self.purchase_order['completion_date'] = completion_date
+        return self
+
+    def get_completion_date(self):
+        return self.purchase_order['completion_date']
--- routes/admin.py
+++ routes/admin.py
@@ -3,6 +3,7 @@ from utils.Database import Database
 
 from models.Item import Item as ItemModel
 from models.User import User as UserModel
+from models.PurchaseOrder import PurchaseOrder as PurchaseOrderModel
 
 admin = Blueprint('admin', __name__)
 
@@ -27,7 +28,25 @@ def transactions():
 def purchase_order():
     if 'privilege' in session:
         if session['privilege'] == 3:
-            return redirect(url_for('admin.Admin'))
+            purchase_orders_pending = PurchaseOrderModel.get_purchase_orders_pending()
+            pending = []
+            for order in purchase_orders_pending:
+                pending_order = {'purchase_order_id': order.get_purchase_order_id(),
+                                 'generated_by': order.get_generated_by(),
+                                 'generated_date': order.get_generated_date()}
+                pending.append(pending_order)
+
+            purchase_orders_history = PurchaseOrderModel.get_purchase_orders_history()
+            history = []
+            for order in purchase_orders_history:
+                history_order = {'purchase_order_id': order.get_purchase_order_id(),
+                                 'generated_by': order.get_generated_by(),
+                                 'generated_date': order.get_generated_date(),
+                                 'completion_date': order.get_completion_date()}
+                history.append(history_order)
+
+            return render_template('purchaseOrder.html', pending=pending,
+                                   history=history)
     return redirect(url_for('admin.Admin'))
 
 
