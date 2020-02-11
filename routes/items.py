1569b0dc31a0f67b5750c72579c22ed74a4eab20
u-blavins <usama.blavins1@gmail.com>
2020-02-11 22:52:09 +0000

    Update api routes to stop info being made public

--- routes/items.py
+++ routes/items.py
@@ -7,29 +7,33 @@ items = Blueprint('items', __name__)
 
 @items.route('/api/items', methods=['GET'])
 def get_items():
-    result = ItemModel.get_all_items()
-    items = []
-    for item in result:
-        items.append(item.__dict__)
-    return jsonify(items=items)
+    if 'user_id' in session:
+        result = ItemModel.get_all_items()
+        items = []
+        for item in result:
+            items.append(item.__dict__)
+        return jsonify(items=items)
 
 
 @items.route('/api/items/item/<code>', methods=['GET'])
 def get_item(code):
-    item = ItemModel.get_item(code)
-    return jsonify(item=item.__dict__)
+    if 'user_id' in session:
+        item = ItemModel.get_item(code)
+        return jsonify(item=item.__dict__)
 
 
 @items.route('/api/items/codes', methods=['GET'])
 def get_codes():
-    codes = ItemModel.get_codes()
-    return jsonify(codes=codes)
+    if 'user_id' in session:
+        codes = ItemModel.get_codes()
+        return jsonify(codes=codes)
 
 
 @items.route('/api/items/<code>/units', methods=['GET'])
 def get_units(code):
-    units_collection = ItemModel.get_unit_types(code)
-    return jsonify(units=units_collection)
+    if 'user_id' in session:
+        units_collection = ItemModel.get_unit_types(code)
+        return jsonify(units=units_collection)
 
 
 @items.route('/items/add', methods=['POST'])
